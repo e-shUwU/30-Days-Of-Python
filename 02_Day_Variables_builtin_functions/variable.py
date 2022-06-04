7c1416c993d988e16dd3c9e3a6470a8602b5f669
Shubham Singh <48835072+e-shUwU@users.noreply.github.com>
2022-06-04 14:53:29 +0530

    Create variable.py

---
+++ 02_Day_Variables_builtin_functions/variable.py
@@ -0,0 +1,38 @@
+#Day 2: 30 Days of python programming
+#level1
+first_name,last_name,full_name,country,city,age,year,is_married,is_true,is_light_on='shubham','singh','shubham singh','India','Derabassi',21,2022,False,False,False
+#level2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+len(full_name)
+Fname_lenght=len(first_name)
+Lname_lenght=len(last_name)
+max(Fname_lenght,Lname_lenght)
+num_one=5
+num_two=4
+var_total=num_one+num_two
+var_diff=num_one-num_two
+var_product=num_one*num_two
+var_division=num_one/num_two
+var_remainder=num_one%num_two
+var_exp=num_one**num_two
+var_floor_division=num_one//num_two
+r=30
+area_of_circle=3.14*r**2
+radius=input('radius of circle ')
+area=3.14*radius**2
+first_name=input('first name: ')
+last_name=input('last name: ')
+country=input('country: ')
+age=input('your age: ')
+help(keywords)
+
+
